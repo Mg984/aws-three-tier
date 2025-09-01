8423473c97cddf2b8be2bfcd28acc64ecae982f8
miracle <miraclegolden78@gmail.com>
2025-09-01 09:45:46 +0100

    deploy

--- 01-provider.tf
+++ 01-provider.tf
@@ -6,19 +6,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-
     kubernetes = {
-      source = "hashicorp/aws"
-      version = "~> 2.20"  
+      source  = "hashicorp/kubernetes"  # Fixed: was pointing to hashicorp/aws
+      version = "~> 2.20"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.10" # Or any version < 2.11
+      version = "~> 2.10"  # Fixed: removed the "Or any version < 2.11" comment
     }
-  
   }
 }
 
